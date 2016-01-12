a9a6ddc9bc3464cb069d07ca0ce9fa7b22b4e77a
Andrey Elagin <daddyingrave@gmail.com>
2016-01-12 22:02:36 +0300

    somecommit

---
+++ classwork/1 11.01/some.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: daddyingrave
+ * Date: 11.01.16
+ * Time: 18:29
+ */
+//$some = 'Hello Satan';
+//var_dump($some);
+//
+//$arr = [1, 2, 3];
+//var_dump($arr);
+//
+//$obj = new stdClass();
+//$obj->name = 'herm';
+//$obj->lastname = 'gugu';
+//var_dump($obj);
+//
+//$fp = fopen('/etc/passwd', 'r');
+//var_dump($fp);
+
+//phpinfo();
+
+var_dump(false);
+
+$someNum = 0;
+var_dump((bool)$someNum);
+
+$someStr = '';
+$someStr2 = '0';
+var_dump((bool)$someStr, (bool)$someStr2);
+
+$arr = [];
+var_dump((bool)$arr);
+
+$someVar;
+var_dump($someVar);
