2847bb21bbd2e1e00ff51104336e32a92c7d8383
Andrey Elagin <daddyingrave@gmail.com>
2016-01-15 18:41:02 +0300

    revert array

--- homework/2.14.01/arrayrevert.php
+++ homework/2.14.01/arrayrevert.php
@@ -1,15 +1,12 @@
 <?php
-
 $someArr = [4, 8, 15, 16, 23, 42, 42];
 $length = (int)((count($someArr)) / 2);
-$a = 0;
-$b = 0;
 
 for ($i = 0; $i <= $length - 1; $i++) {
-   $a = $someArr[$i];
-   $b = $someArr[count($someArr) - 1 - $i];
-   $someArr[$i] = $b;
-   $someArr[count($someArr) - 1 - $i] = $a;
+   $someArr[$i] = $someArr[$i] + $someArr[count($someArr) - 1 - $i];
+   echo $someArr[$i] . "\n";
+   $someArr[count($someArr) - 1 - $i] = $someArr[$i] - $someArr[count($someArr) - 1 - $i];
+   $someArr[$i] = $someArr[$i] - $someArr[count($someArr) - 1 - $i];
 }
 
 print_r($someArr);
